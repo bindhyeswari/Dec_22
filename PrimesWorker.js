bc66380185a31479f43d176687a48b0ad10c917a
pvtech <tech@golivelabs.io>
2014-12-22 13:28:20 -0800

    primes using web workers

--- PrimesWorker.js
+++ PrimesWorker.js
@@ -1,3 +1,22 @@
-/**
- * Created by mishrab on 12/22/14.
- */
+function generatePrimes(numOfPrimes) {
+    var primesArr = [];
+    var counter = 2;
+    while (primesArr.length < numOfPrimes) {
+        if (isPrime(counter)) {
+            primesArr.push(counter);
+        }
+        counter++;
+    }
+    return primesArr;
+}
+
+function isPrime(num) {
+    for (var i = 2, len = Math.sqrt(num); i <= len; i++) {
+        if ( num % i < 1 ) return false;
+    }
+    return true;
+}
+
+self.addEventListener('message', function (event) { // event.data = 'Hello'
+    self.postMessage(generatePrimes(event.data));
+});
